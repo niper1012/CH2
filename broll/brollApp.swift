bf275e7fe976c30cc15e81337252c31071ddc616
niper1012 <niclapersico@hotmail.com>
2025-11-11 14:39:05 +0100

    Uploaded an x code file

---
+++ broll/brollApp.swift
@@ -0,0 +1,17 @@
+//
+//  brollApp.swift
+//  broll
+//
+//  Created by Nicla Persico on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct brollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
